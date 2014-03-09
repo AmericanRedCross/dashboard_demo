a51754764bae36b7b4c98905be8bedbd78937a8c
Dan Joseph <danbjoseph@gmail.com>
2014-03-09 20:21:39 +0800

    adding marker clustering & addtional demo points from formhub survey

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Demonstrating the possibilities of an interactive dashboard of in-depth analyses
 
 **Examples:**
 - Graphing [distributions](http://americanredcross.github.io/dashboard_demo/distributions) over time.
-
+- Interactive [map](http://americanredcross.github.io/dashboard_demo/shelter_recovery) that could be used to show households reached with shelter recovery support
 
 **Technology Used:**
 - [D3.js](http://d3js.org/)
 - [NVD3.js](http://nvd3.org/)
 - [Bootstrap](http://getbootstrap.com/)
 - [jQuery](http://jquery.com/)
+- [Leaflet](http://leafletjs.com/)
+- [Leaflet.markercluster](https://github.com/Leaflet/Leaflet.markercluster)
--- shelter_recovery/js/main.js
+++ shelter_recovery/js/main.js
@@ -7,22 +7,41 @@ var tileLayerUrl = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 var attribution = 'Map data &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a> | Map style by <a href="http://hot.openstreetmap.org" target="_blank">H.O.T.</a> | &copy; <a href="http://redcross.org" title="Red Cross" target="_blank">Red Cross</a> 2013 | <a title="Disclaimer" onClick="showDisclaimer();">Disclaimer</a>';
 var tileLayer = L.tileLayer(tileLayerUrl, {attribution: attribution});
 
-var map = L.map('map', {scrollWheelZoom: false}).setView([11.14899, 125.00064], 17);
-
-tileLayer.addTo(map);
+var map = L.map('map', {
+  scrollWheelZoom: false,
+  zoom: 0,
+  layers: [tileLayer]
+});
 
 var pointData;
+var mapPoints = [];
+var markers = new L.MarkerClusterGroup();
+var markersBounds = [];
+
+var centroidOptions = {
+    radius: 8,
+    fillColor: "#ED1B2E",
+    color: "#FFF",
+    weight: 2.5,
+    opacity: 1,
+    fillOpacity: 1
+};
+
+// reset map bounds using Zoom to Extent button
+function zoomOut() {
+    map.fitBounds(markersBounds);
+}
 
 function getPointData(){
   $.ajax({
       type: 'GET',
-      url: 'data/shelterRecoveryDemo.json',
+      url: 'data/PointDataCollectionDemo_Formhub_2014_03_09_06_57_05.json',
       contentType: 'application/json',
       dataType: 'json',
       timeout: 10000,
       success: function(json) {
         pointData = json;        
-        markersToMap();
+        formatPoints();
       },
       error: function(e) {
           console.log(e);
@@ -30,35 +49,70 @@ function getPointData(){
   });
 }
 
-var featureEvents = function (feature, layer) {
-    layer.on({
-        mouseover: markerInfo,
-        mouseout: defaultInfo
-    });       
+function formatPoints(){
+    $.each(pointData, function(index, item) {
+        latlng = [item._pt_location_longitude, item._pt_location_latitude];
+        var mapPoint = {
+            "type": "Feature",
+            "properties": {
+                "user_select": item.user_select,
+                "user_text": item.user_text,
+                "user_date": item.user_date,                                        
+                "user_integer": item.user_integer,
+                "pt_photo": item.pt_photo  
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": latlng
+            }
+        }
+        mapPoints.push(mapPoint);
+    }); 
+    markersToMap();
 }
 
 function markerInfo(e) {
-    var house = e.target; 
-    $("#houseProjectType").html(house.feature.properties.support);
-    $("#houseFinancingPartner").html(house.feature.properties.partner);
-    $("#houseDateCompleted").html(house.feature.properties.dateEnd);
-    var picPath = "images/houses/" + house.feature.properties.housepic + ".jpg";
-    $("#housePic").attr('src', picPath);
-    $("#defaultSidebar").hide();
-    $(".houseInfo").show();
+  var pt = e.target;
+  $("#user_select").html(pt.feature.properties.user_select);
+  $("#user_text").html(pt.feature.properties.user_text);
+  $("#user_date").html(pt.feature.properties.user_date);
+  var picPath = "images/houses/" + pt.feature.properties.pt_photo;
+  $("#pt_photo").attr('src', picPath);
+  $("#defaultSidebar").hide();
+  $(".ptInfo").show();
 }
 
 function defaultInfo() {
-  $(".houseInfo").hide();
+  $(".ptInfo").hide();
   $("#defaultSidebar").show();
 }
 
-
-
 function markersToMap(){
-  L.geoJson(pointData, {
-    onEachFeature: featureEvents
-  }).addTo(map);
+  map.removeLayer(markers);
+  markers = new L.MarkerClusterGroup({
+      showCoverageOnHover:false, 
+      maxClusterRadius: 15,   
+      spiderfyDistanceMultiplier:2
+  });
+  marker = L.geoJson(mapPoints, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, centroidOptions);
+    },
+    onEachFeature: function (feature, layer) {
+      layer.on({
+        mouseover: markerInfo,
+        mouseout: defaultInfo
+      }); 
+    }         
+  });
+  markers.addLayer(marker);
+  markers.addTo(map);
+  markersBounds = markers.getBounds();
+  markersBounds._northEast.lat += 0.05;
+  markersBounds._northEast.lng += 0.05;
+  markersBounds._southWest.lat -= 0.05;
+  markersBounds._southWest.lat -= 0.05;
+  map.fitBounds(markersBounds);  
 }
 
 getPointData();
